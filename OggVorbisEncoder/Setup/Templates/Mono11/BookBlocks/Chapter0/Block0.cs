750f9fbf830f9e99129175de152930b85e9c7d93
Andrew Au <cshung@gmail.com>
2018-04-15 10:01:57 -0700

    Support encoding mono 11KHz audio

---
+++ OggVorbisEncoder/Setup/Templates/Mono11/BookBlocks/Chapter0/Block0.cs
@@ -0,0 +1,16 @@
+﻿namespace OggVorbisEncoder.Setup.Templates.Mono11.BookBlocks.Chapter0
+{
+    public class Block0 : IStaticBookBlock
+    {
+        public IStaticCodeBook[][] Books { get; } = {
+            new IStaticCodeBook[] {null},
+            new IStaticCodeBook[] {null, null, new Page1()},
+            new IStaticCodeBook[] {null, null, new Page2()},
+            new IStaticCodeBook[] {null, null, new Page3()},
+            new IStaticCodeBook[] {null, null, new Page4()},
+            new IStaticCodeBook[] {null, null, new Page5()},
+            new IStaticCodeBook[] {new Page6_0(), new Page6_1()},
+            new IStaticCodeBook[] {new Page7_0(), new Page7_1(), new Page7_2()},
+        };
+    }
+}
